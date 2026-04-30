1a0db92f115f9628f5ca38ee8dd2d9e5156709e0
sdarawa1 <s_darawad96@yahoo.com>
2026-04-30 14:20:56 -0400

    task 1.2 : Built GUI layout and connected controller actions

--- src/main/java/edu/westga/comp2320/babynamestatistics/controller/BabyNameController.java
+++ src/main/java/edu/westga/comp2320/babynamestatistics/controller/BabyNameController.java
@@ -1,14 +1,64 @@
 package edu.westga.comp2320.babynamestatistics.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
 
 public class BabyNameController {
-    @FXML
-    private Label welcomeText;
+
+    // Buttons
+    @FXML private Button addButton;
+    @FXML private Button deleteAllButton;
+    @FXML private Button deleteButton;
+    @FXML private Button searchButton;
+
+    // Radio Buttons
+    @FXML private RadioButton maleRadio;
+    @FXML private RadioButton femaleRadio;
+
+    // Text Fields
+    @FXML private TextField nameField;
+    @FXML private TextField yearField;
+    @FXML private TextField frequencyField;
+
+    // ListView
+    @FXML private ListView<String> recordListView; // we'll fix type later
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onSearchClick() {
+        System.out.println("Search clicked");
+    }
+
+    @FXML
+    private void onAddClick() {
+        System.out.println("Add clicked");
+    }
+
+    @FXML
+    private void onDeleteClick() {
+        System.out.println("Delete clicked");
+    }
+
+    @FXML
+    private void onDeleteAllClick() {
+        System.out.println("Delete All clicked");
+    }
+
+    @FXML
+    private void onOpenClick() {
+        System.out.println("Open clicked");
+    }
+
+    @FXML
+    private void onSaveClick() {
+        System.out.println("Save clicked");
+    }
+
+    @FXML
+    private void onAboutClick() {
+        System.out.println("About clicked");
     }
 }
