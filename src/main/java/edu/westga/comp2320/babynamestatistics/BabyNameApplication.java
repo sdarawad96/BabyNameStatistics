f49e2f6e12b5d4c5d860eea6cd3b94ba64e4859d
sdarawa1 <s_darawad96@yahoo.com>
2026-05-01 15:15:22 -0400

    edited scene size

--- src/main/java/edu/westga/comp2320/babynamestatistics/BabyNameApplication.java
+++ src/main/java/edu/westga/comp2320/babynamestatistics/BabyNameApplication.java
@@ -11,7 +11,7 @@ public class BabyNameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BabyNameApplication.class.getResource("babyname-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 567, 581);
+        Scene scene = new Scene(fxmlLoader.load(), 645, 655);
         stage.setTitle("Baby Name Statistics by Sondus Darawad");
         stage.setScene(scene);
         stage.show();
